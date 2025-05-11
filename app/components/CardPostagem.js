72d271984bbcb1631f12c9946a3503fb20de1386
Vinicius Rivaldar dos Santos Silva <149613214+ViniRivaldar@users.noreply.github.com>
2025-05-11 00:16:16 +0000

    adicionando e editando os botões de editar e excluir

--- app/components/CardPostagem.js
+++ app/components/CardPostagem.js
@@ -1,35 +1,51 @@
 import Link from "next/link"
 import Image from "next/image"
+import { FaTrash, FaEdit } from 'react-icons/fa'
 
 export default function CardPostagem(){
     return(
-        <Link href='./post/1'
-            className="flex justify-center gap-10 bg-[#D3D3D3] mb-10 p-10
+        <div className="flex flex-col gap-2 bg-[#D3D3D3] mb-10 p-10
             transition-all duration-300 hover:bg-[#c0c0c0] hover:shadow-lg 
             hover:-translate-y-1 rounded"
         >
-            <Image
-            src='/download.jpg'
-            alt="imagen"
-            width={499}
-            height={153}
-            />
-            <div className="w-[499px] h-[153px] flex flex-col">
-                <h1 className="font-openSans text-primary text-center text-[32px] 
-                    font-bold leading-[30px] mb-5"
+
+            <Link href='./post/1' className="flex justify-center gap-10">
+                <Image
+                src='/download.jpg'
+                alt="imagen"
+                width={499}
+                height={153}
+                />
+                <div className="w-[499px] h-[153px] flex flex-col">
+                    <h1 className="font-openSans text-primary text-center text-[32px] 
+                        font-bold leading-[30px] mb-5"
+                    >
+                        Treino do Corinthians: Memphis volta a trabalhar 
+                        com bola e pode ser reforço contra o Inter
+                    </h1>
+
+                    <p className="font-openSans text-primary text-[16px] font-normal">
+                        Atacante é liberado para treinar com o restante 
+                        do grupo depois de se recuperar dos traumas sofridos 
+                        no pé direito e perna direita; Gustavo Henrique 
+                        passará por cirurgia e é baixa
+                    </p>
+                </div>
+            </Link>
+
+            <div className="flex justify-end gap-2">
+                <button 
+                className="p-2 bg-blue-500 text-white rounded hover:bg-blue-400 cursor-pointer"
                 >
-                    Treino do Corinthians: Memphis volta a trabalhar 
-                    com bola e pode ser reforço contra o Inter
-                </h1>
-
-                <p className="font-openSans text-primary text-[16px] font-normal">
-                    Atacante é liberado para treinar com o restante 
-                    do grupo depois de se recuperar dos traumas sofridos 
-                    no pé direito e perna direita; Gustavo Henrique 
-                    passará por cirurgia e é baixa
-                </p>
-
+                <FaEdit size={20} />
+                </button>
+                <button 
+                className="p-2 bg-red-500 text-white rounded hover:bg-red-400 cursor-pointer"
+                >
+                <FaTrash size={20} />
+                </button>
             </div>
-        </Link>
+        </div>
+        
     )
 }
--- app/page.js
+++ app/page.js
@@ -5,7 +5,15 @@ export default function Home() {
   return (
     <div className="w-[1192px] bg-[#191970] mt-10 mb-10 p-10 rounded">
       <h1 className="font-anton font-bold text-5xl text-white text-center mb-10"> Postagens </h1>
-      <Link href='/criarpostagem'>CriarPostagem</Link>
+      <Link 
+        href='/criarpostagem' 
+        className="w-[200px] h-[50px] bg-[#D3D3D3] p-5 text-black
+        flex items-center justify-center rounded 
+        hover:bg-black hover:text-white transition-all duration-300
+        hover:shadow-lg hover:-translate-y-1 cursor-pointer mb-5"
+      >
+        Criar Postagem
+      </Link>
       <CardPostagem/>
     </div>
   );
