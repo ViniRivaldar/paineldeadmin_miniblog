b36d387fb4ee5b184b5238490023116036c86315
Vinicius Rivaldar dos Santos Silva <149613214+ViniRivaldar@users.noreply.github.com>
2025-05-17 14:01:29 +0000

    puxando dados da api

--- app/components/CardPostagem.js
+++ app/components/CardPostagem.js
@@ -1,46 +1,65 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image"
 import { FaTrash} from 'react-icons/fa'
+import { useState, useEffect } from "react"
+
+import { axiosPots } from "@/config/Axios"
 
 export default function CardPostagem(){
-    return(
-        <div className="flex flex-col gap-2 bg-[#D3D3D3] mb-10 p-10
+    const [isPost, setIsPost] = useState([])
+
+    useEffect(()=>{
+        async function fecth (){
+            const response = await axiosPots('/posts')
+            setIsPost(response.data)
+        } 
+
+        fecth()
+        
+    },[])
+
+
+    return (
+    <>
+      {isPost.map((post) => (
+        <div
+          key={post.id}
+          className="flex flex-col gap-2 bg-[#D3D3D3] mb-10 p-10
             transition-all duration-300 hover:bg-[#c0c0c0] hover:shadow-lg 
             hover:-translate-y-1 rounded"
         >
-
-            <Link href='./post/1' className="flex justify-center gap-10">
-                <Image
-                src='/download.jpg'
-                alt="imagen"
+          <Link href={`/post/${post.id}`} className="flex justify-center gap-10">
+            {post.fotos && post.fotos[0]?.url && (
+              <Image
+                src={post.fotos[0].url}
+                alt="imagem do post"
                 width={499}
                 height={153}
-                />
-                <div className="w-[499px] h-[153px] flex flex-col">
-                    <h1 className="font-openSans text-primary text-center text-[32px] 
-                        font-bold leading-[30px] mb-5"
-                    >
-                        Treino do Corinthians: Memphis volta a trabalhar 
-                        com bola e pode ser reforço contra o Inter
-                    </h1>
+                className="rounded"
+              />
+            )}
 
-                    <p className="font-openSans text-primary text-[16px] font-normal">
-                        Atacante é liberado para treinar com o restante 
-                        do grupo depois de se recuperar dos traumas sofridos 
-                        no pé direito e perna direita; Gustavo Henrique 
-                        passará por cirurgia e é baixa
-                    </p>
-                </div>
-            </Link>
-
-            <div className="flex justify-end">
-                <button 
-                className="p-2 bg-red-500 text-white rounded hover:bg-red-400 cursor-pointer"
-                >
-                <FaTrash size={20} />
-                </button>
+            <div className="w-[499px] h-[153px] flex flex-col justify-center">
+              <h1
+                className="font-openSans text-primary text-center text-[32px] 
+                  font-bold leading-[30px]"
+              >
+                {post.title}
+              </h1>
             </div>
+          </Link>
+
+          <div className="flex justify-end">
+           <button 
+                className="p-2 bg-red-500 text-white rounded hover:bg-red-400 cursor-pointer"
+            >
+                <FaTrash size={20} />
+            </button>
+          </div>
         </div>
-        
-    )
+      ))}
+    </>
+  )
 }
