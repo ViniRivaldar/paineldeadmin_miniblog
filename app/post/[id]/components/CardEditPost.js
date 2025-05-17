283a43f7fd7c99de59d99cb7964b12e9be9c1911
Vinicius Rivaldar dos Santos Silva <149613214+ViniRivaldar@users.noreply.github.com>
2025-05-17 14:21:38 +0000

    criando componente de editpost

--- app/post/[id]/components/CardEditPost.js
+++ app/post/[id]/components/CardEditPost.js
@@ -1,247 +1,40 @@
-import Image from "next/image"
-import Link from "next/link"
-import { FaArrowLeft } from "react-icons/fa"
+import Image from "next/image";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
-export default function CardEditPost(){
-    return(
-        <div 
-        className="relative w-[1192px] bg-[#D3D3D3] 
-        mt-10 mb-10 p-10 flex flex-col items-center gap-10 rounded"
-        >
+export default function CardEditPost({ post }) {
+  return (
+    <div className="relative w-[1192px] bg-[#D3D3D3] mt-10 mb-10 p-10 flex flex-col items-center gap-10 rounded">
+      <div className="absolute top-7 left-10">
+        <Link href='/'>
+          <FaArrowLeft className='w-6 h-6 hover:text-white text-black' />
+        </Link>
+      </div>
 
-            
-            <div className="absolute top-7 left-10">
-                <Link href='/'>
-                    <FaArrowLeft className='w-6 h-6 hover:text-white text-black' />
-                </Link>
-            </div>
+      {post.fotos && post.fotos[0]?.url && (
+        <Image
+          src={post.fotos[0].url}
+          alt="imagem do post"
+          width={800}
+          height={500}
+          className="rounded"
+        />
+      )}
 
-            <Image
-            src='/download.jpg'
-            alt="imagen"
-            width={800}
-            height={0}
-            className=" rounded"
-            />
-            <div>
-                <h1
-                className="text-[#070D21] text-center font-anton text-[48px] font-normal leading-none"
-                >Título do Blog: A Arte da Escrita Criativa</h1>
-                <br/>
-                <p 
-                className="text-black font-openSans 
-                text-[16px] font-bold leading-none"
-                >Publicado em 2 de maio de 2025</p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-                <p className="text-primary font-openSans text-[16px] leading-none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla consectetur, lacus vel tincidunt fermentum, orci leo 
-                cursus est, a luctus ligula justo nec augue. Donec nec 
-                efficitur risus. Sed consequat porta leo. Vestibulum ante 
-                ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia curae; Sed tincidunt lorem at magna porta, in mattis 
-                magna pulvinar. Integer feugiat arcu vel est imperdiet, 
-                vitae commodo risus scelerisque. Curabitur vehicula ante sed 
-                est bibendum, nec bibendum nulla malesuada.
-                </p><br/>
-            </div>
-        </div>
-    )
+      <div>
+        <h1 className="text-[#070D21] text-center font-anton text-[48px] font-normal leading-none">
+          {post.title}
+        </h1>
+        <br />
+        <p className="text-black font-openSans text-[16px] font-bold leading-none">
+          Publicado em {new Date(post.createdAt).toLocaleDateString('pt-BR')}
+        </p>
+        <br />
+        <div
+          className="text-primary font-openSans text-[16px] leading-relaxed"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </div>
+    </div>
+  );
 }
--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,9 +1,14 @@
-import CardEditPost from "./components/CardEditPost"
 
-export default function Post(){
-    return(
-        <>
-            <CardEditPost/>
-        </>
-    )
+import { axiosPots } from "@/config/Axios";
+import CardEditPost from "./components/CardEditPost";
+
+export default async function PostPage({ params }) {
+  const { id } = params;
+
+  console.log(id)
+  
+  const response = await axiosPots(`/posts/${id}`);
+  const post = response.data;
+
+  return <CardEditPost post={post} />;
 }
